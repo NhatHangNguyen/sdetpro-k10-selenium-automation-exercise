e341c46acc6708897809f67fca69e26b9f89c5f3
Hang Chau Nhat Nguyen <hangcnnguyen@kms.com.vn>
2022-11-18 16:15:09 +0700

    lesson-19

--- src/test/java/url/Urls.java
+++ src/test/java/url/Urls.java
@@ -2,10 +2,12 @@ package url;
 
 public interface Urls {
 
-    String BASE_URL = "https://the-internet.herokuapp.com/";
+    String BASE_URL = "https://the-internet.herokuapp.com";
+    String LOGIN_SLUG = "/login";
     String DROPDOWN_SLUG = "/dropdown";
     String IFRAME_SLUG = "/iframe";
     String HOVERS_SLUG = "/hovers";
     String DYNAMIC_CONTROL_SLUG = "/dynamic_controls";
-
+    String JS_ALERT_SLUG = "/javascript_alerts";
+    String FLOATING_MENU_SLUG = "/floating_menu";
 }
